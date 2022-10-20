0b8889c7bf31023139e149e5130da7e4b29e0bee
Gabriel <gabrielferrari601@gmail.com>
2022-10-20 19:17:53 -0300

    Feat(insights.py): cria funçao que verifica salario compativel

--- src/insights.py
+++ src/insights.py
@@ -3,7 +3,7 @@ from src.jobs import read
 
 def get_unique_job_types(path):
     jobList = read(path)
-    jobTypeSet = {jobs['job_type'] for jobs in jobList}
+    jobTypeSet = {jobs["job_type"] for jobs in jobList}
     # jobTypeSet = set()
     # for jobs in jobList:
     #     jobTypeSet.add(jobs['job_type'])
@@ -11,40 +11,63 @@ def get_unique_job_types(path):
 
 
 def filter_by_job_type(jobs, job_type):
-    return [job for job in jobs if job['job_type'] == job_type]
+    return [job for job in jobs if job["job_type"] == job_type]
 
 
 def get_unique_industries(path):
     jobList = read(path)
     jobIndustries = {
-        jobs['industry'] for jobs in jobList if len(jobs['industry']) > 0
-        }
+        jobs["industry"] for jobs in jobList if len(jobs["industry"]) > 0
+    }
     # print(jobIndustries)
     return jobIndustries
 
 
 def filter_by_industry(jobs, industry):
-    return [job for job in jobs if job['industry'] == industry]
+    return [job for job in jobs if job["industry"] == industry]
 
 
 def get_max_salary(path):
     jobList = read(path)
     jobSalaries = [
-        int(salary['max_salary'])
-        for salary in jobList if len(salary['max_salary']) > 0
-        and salary['max_salary'] != 'invalid'
-        ]
+        int(salary["max_salary"])
+        for salary in jobList
+        if len(salary["max_salary"]) > 0 and salary["max_salary"] != "invalid"
+    ]
     return max(jobSalaries)
 
 
 def get_min_salary(path):
     jobList = read(path)
     jobSalaries = [
-        int(salary['min_salary'])
-        for salary in jobList if len(salary['min_salary']) > 0
-        and salary['min_salary'] != 'invalid'
-        ]
+        int(salary["min_salary"])
+        for salary in jobList
+        if len(salary["min_salary"]) > 0 and salary["min_salary"] != "invalid"
+    ]
     return min(jobSalaries)
+
+
+def it_min_and_max_salary_exists(job):
+    if "min_salary" not in job or "max_salary" not in job:
+        raise ValueError("One of the ranges doesn't exists")
+
+
+def is_numeric_value_range_salary(job):
+    if (
+        not str(job["min_salary"]).isnumeric()
+        or not str(job["max_salary"]).isnumeric()
+    ):
+        raise ValueError("One of the ranges is non-numeric")
+
+
+def does_salary_range_makes_sense(job):
+    if job["min_salary"] > job["max_salary"]:
+        raise ValueError("Min value is greater than max")
+
+
+def is_numeric_value_salary(salary):
+    if type(salary) != int:
+        raise ValueError("Non numeric salary")
 
 
 def matches_salary_range(job, salary):
@@ -70,7 +93,13 @@ def matches_salary_range(job, salary):
         If `job["min_salary"]` is greather than `job["max_salary"]`
         If `salary` isn't a valid integer
     """
-    pass
+    it_min_and_max_salary_exists(job)
+    is_numeric_value_range_salary(job)
+    does_salary_range_makes_sense(job)
+    is_numeric_value_salary(salary)
+    if job["min_salary"] <= salary <= job["max_salary"]:
+        return True
+    return False
 
 
 def filter_by_salary_range(jobs, salary):
