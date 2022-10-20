96eaaf8c1c9616ad33e1303d60f90660fb3d0ce0
Gabriel <gabrielferrari601@gmail.com>
2022-10-20 17:41:47 -0300

    Feat(insights.py): cria funçao q retorna maior salario

--- src/insights.py
+++ src/insights.py
@@ -59,21 +59,13 @@ def filter_by_industry(jobs, industry):
 
 
 def get_max_salary(path):
-    """Get the maximum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    pass
+    jobList = read(path)
+    jobSalaries = [
+        int(salary['max_salary'])
+        for salary in jobList if len(salary['max_salary']) > 0
+        and salary['max_salary'] != 'invalid'
+        ]
+    return max(jobSalaries)
 
 
 def get_min_salary(path):
