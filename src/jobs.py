8cc281fbc8fc7c3195abff75b69626ecd8cae090
Gabriel <gabrielferrari601@gmail.com>
2022-10-20 15:11:29 -0300

    Feat(jobs.py): Cria lista de jobs

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,18 @@
 from functools import lru_cache
-
+import csv
 
 @lru_cache
 def read(path):
     """Reads a file from a given path and returns its contents
-
     Parameters
     ----------
     path : str
         Full path to file
-
     Returns
     -------
     list
         List of rows as dicts
     """
-    return []
+    file = open(path, encoding='utf8')
+    data = csv.DictReader(file, delimiter=",", quotechar='"')
+    return [row for row in data]
